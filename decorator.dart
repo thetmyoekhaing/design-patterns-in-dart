827172a16f4e37550429b2f2f5993482cf7e9675
tomari99 <thetmyoekhaing.npi@gmail.com>
2023-07-10 17:09:29 +0630

    decorator design pattern done

---
+++ decorator.dart
@@ -0,0 +1,83 @@
+abstract class Burger {
+  String getDescription();
+  double getCost();
+}
+
+class PlainBurger implements Burger {
+  @override
+  String getDescription() {
+    return 'tomatoes, onions, lettace, pickles, mustard, ketchup, bacon';
+  }
+
+  @override
+  double getCost() {
+    return 5;
+  }
+}
+
+abstract class Topping implements Burger {
+  PlainBurger burger;
+  Topping({required this.burger});
+  @override
+  double getCost() {
+    return burger.getCost();
+  }
+
+  @override
+  String getDescription() {
+    return burger.getDescription();
+  }
+}
+
+class AddTopping extends Topping {
+  final String toppingName;
+  final double toppingCost;
+  AddTopping({
+    required super.burger,
+    required String this.toppingName,
+    required double this.toppingCost,
+  });
+
+  @override
+  double getCost() {
+    return super.getCost() + toppingCost;
+  }
+
+  @override
+  String getDescription() {
+    print("Added $toppingName");
+    return super.getDescription() + ', ' + toppingName;
+  }
+}
+
+class ToppingModel {
+  ToppingModel({required this.toppingCost, required this.toppingName});
+  final String toppingName;
+  final double toppingCost;
+}
+
+void main() {
+  PlainBurger plainBurger = PlainBurger();
+  final cheese = ToppingModel(toppingCost: 0.3, toppingName: 'Cheese');
+  final egg = ToppingModel(toppingCost: 0.5, toppingName: 'Egg');
+
+  print('Plain Burger Cost ${plainBurger.getCost()}');
+  print('Plain Burger Description ${plainBurger.getDescription()}');
+
+  AddTopping cheeseTopping = AddTopping(
+    burger: plainBurger,
+    toppingName: cheese.toppingName,
+    toppingCost: cheese.toppingCost,
+  );
+
+  print('Plain Burger Description ${cheeseTopping.getDescription()}');
+  print('Plain Burger with cheese Cost ${cheeseTopping.getCost()}');
+
+  AddTopping eggTopping = AddTopping(
+    burger: plainBurger,
+    toppingName: egg.toppingName,
+    toppingCost: egg.toppingCost,
+  );
+  print('Plain Burger Description ${eggTopping.getDescription()}');
+  print('Plain Burger with cheese and egg Cost ${eggTopping.getCost()}');
+}
